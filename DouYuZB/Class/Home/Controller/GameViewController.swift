3810fe6aaa089db00b8729161196e595a2430dd4
songlei <815484983@qq.com>
2016-10-29 08:59:11 +0800

    添加游戏控制器
    添加游戏控制器

---
+++ DouYuZB/Class/Home/Controller/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  DouYuZB
+//
+//  Created by songlei on 2016/10/28.
+//  Copyright © 2016年 songlei. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.red
+        
+        
+        
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
